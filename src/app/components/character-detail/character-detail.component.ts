029640e46f48157ef4129aee50defe1a73017c8d
Javier <javier.romero.7e3@itb.cat>
2022-02-09 20:39:49 +0100

    Store and delete favourite characters from detail page

--- src/app/components/character-detail/character-detail.component.ts
+++ src/app/components/character-detail/character-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { DataService } from '../../services/data.service';
+import { DataLocalService } from '../../services/data-local.service';
 
 @Component({
   selector: 'app-character-detail',
@@ -12,15 +13,26 @@ export class CharacterDetailComponent implements OnInit {
   @Input() id
 
   character;
+  comics = [];
+  btn_text = ['Añadir a favoritos', 'Eliminar de favoritos']
+  favourite = false;
 
-  constructor(private modalCtrl: ModalController, private data: DataService) { }
+  constructor(private modalCtrl: ModalController, private data: DataService, private dataLocal: DataLocalService) { }
 
   ngOnInit() {
     this.data.getCharacterDetails(this.id).subscribe(
       resp =>{
         this.character = resp.data.results[0];
+        this.comics = resp.data.results[0].comics.items;
       }
     )
+    this.dataLocal.getFavCharacters().then(data =>{
+      for(let character of data){
+        if(this.id == character.id){
+          this.favourite = true;
+        }
+      }
+    })
   }
 
   goBack(){
@@ -29,6 +41,23 @@ export class CharacterDetailComponent implements OnInit {
 
   getCharacterImage(character){
     return character.thumbnail.path + '/landscape_xlarge.' + character.thumbnail.extension;
+  }
+
+  storeFavCharacter(character){
+    this.favourite = true;
+    console.log('PERSONAJE: ' + character.name)
+    this.dataLocal.setFavCharacter(character)
+  }
+
+  getFavButtonIcon(){
+    var icon = ''
+    this.favourite ? icon = 'heart-dislike-outline' : icon = 'heart-outline';
+    return icon;
+  }
+
+  unsetFavCharacter(character){
+    this.dataLocal.unsetFavCharacters(character);
+    this.favourite = false;
   }
 
 }
